c41d7333172b378c50abff6ef407d8e9961f0c76
Vahn Gomes <me@vahngomes.dev>
2023-09-05 12:16:24 -0400

    Added new func

--- src/utils/SeederTool.cpp
+++ src/utils/SeederTool.cpp
@@ -7,13 +7,16 @@
  * MIT Licensed
  */
 
-#include "SeederTool.h"
+#include "SeederTool.h" // SeederTool Utility
+#include <unistd.h>     // For getting the current working directory
 
 // Variables
 string appDir;
 string tempDir;
 
-// Print banner
+/**
+ * Display the banner
+ */
 void printBanner()
 {
     cout << "#######################################\n";
@@ -23,4 +26,34 @@ void printBanner()
     cout << "#######################################\n\n";
     cout << "This utility will help you seed your DynamoDB tables with data.\n";
     cout << "---------------------------------------\n\n";
+}
+
+/**
+ * Helper function to get the current working directory
+ *
+ * @param path The path to check
+ *
+ * @return string The path
+ */
+string pathHelper(string path)
+{
+    // If the path is ./ or . or ../
+    if (path == "./" || path == "." || path == "../")
+    {
+        char currentPath[FILENAME_MAX];
+        if (getcwd(currentPath, sizeof(currentPath)) != nullptr)
+        {
+            return currentPath;
+        }
+        else
+        {
+            return "";
+        }
+    }
+    // If the path is not ./ or . or ../
+    else
+    {
+        // Return the path
+        return path;
+    }
 }
