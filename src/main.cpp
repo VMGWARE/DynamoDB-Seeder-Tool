a706e46e996fd647a1550e517a84b499dcda89d6
Vahn Gomes <me@vahngomes.dev>
2023-09-05 12:16:16 -0400

    Added temp content

---
+++ src/main.cpp
@@ -0,0 +1,42 @@
+
+/*!
+ * DynamoDB Seeder Tool
+ * https://vmgware.dev/
+ *
+ * Copyright (c) 2023 VMG Ware
+ * MIT Licensed
+ */
+
+// Includes
+#include <iostream> // I/O
+#include <string> // String
+#include <vector> // Vector
+#include <fstream> // Filestream
+#include "utils/SeederTool.h" // SeederTool Utility
+// TODO: Include AWS SDK for C++ (DynamoDB)
+
+// Namespaces
+using namespace std;
+
+// Main function
+int main(int argc, char *argv[])
+{
+    // If possible have a checker run to see if being ran via CLI
+    // Or if being ran via GUI
+
+    // If being ran via CLI
+
+    // Print banner
+    printBanner();
+
+    // Check if DynamoDB is running
+
+    // Make sure the required parameters are passed in
+    // If not we can print the help menu and exit
+
+    // If the required parameters are passed in
+    // Check if the folder passed exists, if not throw an exception
+
+    // Return
+    return 0;
+}
